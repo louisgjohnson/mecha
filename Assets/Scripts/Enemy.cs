9b949a2430e5e44c50479e3eda7affdd70bc8726
louisgjohnson <louisgjohnson@gmail.com>
2020-06-26 21:17:33 +1000

    updated player movement - added enemy movement

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    float knockbackTime = 0.35f;
+    float knockbackForce = 5f;
+    float knockbackCounter = 0f;
+    float gravityValue = -9.81f;
+
+    Vector3 moveDirection = Vector3.zero;
+
+    private CharacterController controller;
+
+    GameObject target;
+
+    void Awake() {
+        gameObject.tag = "Enemy";
+    }
+
+    void Start() {
+        controller = gameObject.AddComponent<CharacterController>();
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+   int health = 3;
+   public void ApplyDamage(int damage, GameObject player) {
+       health -= damage;
+       if (health <= 0) {
+          // Destory enemy
+       }
+       
+       Knockback(player.transform.forward);
+   }
+
+   void Update() {
+       if (knockbackCounter <= 0) {
+           // moveDirection = Vector3.Lerp(moveDirection, Vector3.zero, Time.deltaTime * 3);
+           moveDirection = (target.transform.position - transform.position) * 0.5f;
+       } else {
+           knockbackCounter -= Time.deltaTime;
+       }
+       controller.Move(moveDirection * Time.deltaTime);
+   }
+
+
+   public void Knockback(Vector3 direction) {
+       knockbackCounter = knockbackTime;
+       moveDirection = direction * knockbackForce;
+   }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,8 +12,19 @@ public class PlayerController : MonoBehaviour
     private float jumpHeight = 1.0f;
     private float gravityValue = -9.81f;
 
+    [SerializeField]
+    float boostTimer = 0.15f;
+    [SerializeField]
+    float boostForce = 1.1f;
+    float boostCounter = 0f;
+    Vector3 boostVelocity = Vector3.zero;
+
     GameObject target = null;
     int target_index = 0;
+
+    void Awake() {
+        gameObject.tag = "Player";
+    }
 
     private void Start()
     {
@@ -55,8 +66,9 @@ public class PlayerController : MonoBehaviour
         {
             SwapTarget();
         }
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        
+      
 
         if (target)
         {
@@ -67,8 +79,29 @@ public class PlayerController : MonoBehaviour
             Debug.DrawRay(transform.position, newDirection, Color.red);
             transform.forward = newDirection;
 
+            if (Input.GetKeyDown(KeyCode.V) && Vector3.Distance(transform.position, target.transform.position) <= 2.0f) {
+                target.GetComponent<Enemy>().ApplyDamage(1, gameObject);
+            }
         }
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
+
+        Vector3 move = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        move.Normalize();
+        Vector3 direction = move;
+
+        if (Input.GetKeyDown(KeyCode.L)) {
+            Boost(move);
+        } 
+
+        if (boostCounter > 0) {
+            boostCounter -= Time.deltaTime;
+            direction = boostVelocity;
+        }
+
+        controller.Move(direction * Time.deltaTime * playerSpeed);
     }
+
+    public void Boost(Vector3 direction) {
+       boostCounter = boostTimer;
+       boostVelocity = direction * boostForce;
+   }
 }
